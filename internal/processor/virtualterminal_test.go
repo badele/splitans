f610e5c0e10f9b88386f93001aa5a0218354525b
Bruno Adele <brunoadele@gmail.com>
2026-02-13 20:10:03 +0100

    feat: ignore null & space character (#22)

--- internal/processor/virtualterminal_test.go
+++ internal/processor/virtualterminal_test.go
@@ -289,14 +289,28 @@ func TestGetContentBounds(t *testing.T) {
 			},
 		},
 		{
-			name:   "Only spaces (counted as content)",
+			name:   "Only default spaces (ignored)",
 			width:  10,
 			height: 5,
 			tokens: []types.Token{
 				{Type: types.TokenText, Value: "   "},
 			},
+			expected: ContentBounds{Empty: true},
+		},
+		{
+			name:   "Count styled spaces",
+			width:  10,
+			height: 5,
+			tokens: []types.Token{
+				{Type: types.TokenText, Value: " "},
+				{Type: types.TokenText, Value: "A"},
+				{Type: types.TokenText, Value: " "},
+				{Type: types.TokenSGR, Parameters: []string{"41"}},
+				{Type: types.TokenText, Value: " "},
+				{Type: types.TokenSGR, Parameters: []string{"0"}},
+			},
 			expected: ContentBounds{
-				MinX: 0, MaxX: 2, MinY: 0, MaxY: 0,
+				MinX: 1, MaxX: 3, MinY: 0, MaxY: 0,
 				Width: 3, Height: 1, Empty: false,
 			},
 		},
